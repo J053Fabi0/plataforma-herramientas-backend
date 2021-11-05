3ee3246f11cc620f0d9b8e8660d96d93d569cfee
Amiko <senorbinario@tuta.io>
2021-11-04 21:37:29 -0700

    Los nombres de las etiquetas deben ser únicos

--- Utils/Models/tagsModel.js
+++ Utils/Models/tagsModel.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const tagsSchema = new Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, unique: true, required: true },
     category: { type: Schema.Types.ObjectId, ref: "Categories" }, // https://mongoosejs.com/docs/populate.html
   },
   {
