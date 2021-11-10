4e10bdac8b7490aea3b612fb16469faa588570d0
Amiko <senorbinario@tuta.io>
2021-11-09 18:32:32 -0700

    La conexión a la base de datos será por .env

--- Utils/Database/connection.js
+++ Utils/Database/connection.js
@@ -3,15 +3,12 @@ const mongoose = require("mongoose");
 // Solo se conecta a mongodb si no se está haciendo un test
 if (process.env.NODE_ENV !== "test") {
   console.log("Connecting to DB...");
-  mongoose.connect(
-    "mongodb+srv://LxpLabPruebas:Josafat123!!@lxplabspruebas.nhq1f.mongodb.net/lxp-lab-Experiences?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  );
+  mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 }
 
 module.exports = mongoose;
